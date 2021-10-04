e10d5699010ee4053e3944078d8ab8c918e2865f
KendallBurton <55636959+KendallBurton@users.noreply.github.com>
2021-10-04 02:41:09 -0400

    query update

--- app/src/main/java/com/example/instagram_clone_android/ParseApplication.java
+++ app/src/main/java/com/example/instagram_clone_android/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.instagram_clone_android;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("5MM8WCfd1qv1d3qpvQitmpvyShhkNff0GQ3oE57R")
