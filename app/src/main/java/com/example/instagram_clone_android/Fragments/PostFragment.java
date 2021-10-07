d82a6fb42b51e6665ab6e674548bd9ea24b635a8
KendallBurton <55636959+KendallBurton@users.noreply.github.com>
2021-10-07 10:08:53 -0400

    Recycler view

---
+++ app/src/main/java/com/example/instagram_clone_android/Fragments/PostFragment.java
@@ -0,0 +1,73 @@
+package com.example.instagram_clone_android.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instagram_clone_android.Post;
+import com.example.instagram_clone_android.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PostFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PostFragment extends Fragment {
+
+    private RecyclerView rvPosts;
+    public static final String TAG = "PostFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // Steps to use the recycler view
+        // 0. Create layout for one row in the list
+        // 1. create adapter
+        // 2. create the data source
+        // 3. set the adapter on the recycler view
+        // 4. set the layout manager  on the recycler view
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+
+
+
+}
